43e4bfa55aadf02e715c6ef71ed339800d4343ca
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-03-28 11:23:54 +0530

    [MANVI GROVER] ADD: Reversed a list without built in methods

--- src/main/java/com/bridgelabz/listInterface/ReverseList.java
+++ src/main/java/com/bridgelabz/listInterface/ReverseList.java
@@ -1,5 +1,14 @@
 package com.bridgelabz.listInterface;
 
-public class ReverseList {
+import java.util.*;
 
+public class ReverseList {
+    public static void main(String[] args) {
+        ArrayList<Integer>list=new ArrayList<>(Arrays.asList(1,2,3,4,5));
+        ArrayList<Integer>reversedList=new ArrayList<>();
+        for(int i=list.size()-1;i>=0;i--){
+            reversedList.add(list.get(i));
+        }
+        System.out.println("reversed list is: "+reversedList);
+    }
 }
