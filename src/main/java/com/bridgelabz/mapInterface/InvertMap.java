ddc9f7a34c93a75cb6f79d480a63a9547f3c3ca9
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-03-31 00:05:40 +0530

    [MANVI GROVER] ADD: Inverting map by handling dupliacte values by storing them in a list.

---
+++ src/main/java/com/bridgelabz/mapInterface/InvertMap.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.mapInterface;
+
+import java.util.*;
+
+public class InvertMap {
+    public static <K, V> Map<V, List<K>> invertMap(Map<K, V> map) {
+        Map<V, List<K>> invertedMap = new HashMap<>();
+
+        for (Map.Entry<K, V> entry : map.entrySet()) {
+            invertedMap.computeIfAbsent(entry.getValue(), k -> new ArrayList<>()).add(entry.getKey());
+        }
+
+        return invertedMap;
+    }
+
+    public static void main(String[] args) {
+        Map<String, Integer> originalMap = new HashMap<>();
+        originalMap.put("A", 1);
+        originalMap.put("B", 2);
+        originalMap.put("C", 1);
+
+        Map<Integer, List<String>> inverted = invertMap(originalMap);
+        System.out.println(inverted);
+    }
+}
+
